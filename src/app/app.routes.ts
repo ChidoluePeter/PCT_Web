1916adda72866afcfbdbe03bf427774f42b0de7f
ChidoluePeter <peterchidolue@gmail.com>
2025-02-04 12:51:22 +0100

    Learning UI Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { LearningComponent } from './learning/learning.component';
 
 
 
@@ -15,6 +16,9 @@ export const routes: Routes = [
   {
 
     path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path:'learning', component:LearningComponent
   }
 ];
 
