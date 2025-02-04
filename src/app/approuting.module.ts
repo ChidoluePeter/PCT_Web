06b07451300fcd96f6048eb0d330fbb78f478738
ChidoluePeter <peterchidolue@gmail.com>
2025-02-04 13:01:27 +0100

    update

--- src/app/approuting.module.ts
+++ src/app/approuting.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { LearningComponent } from './learning/learning.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component:DashboardComponent}
+  { path: 'dashboard', component:DashboardComponent},
+  {path: 'learning', component:LearningComponent}
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,6 +16,7 @@ export class DashboardComponent {
 
   learning(){
   this.router.navigate(['learning'])
+ 
 }
 
 
