6ea0fca9b0aa1e9f401cd5b22aada5c1c94808e7
ChidoluePeter <peterchidolue@gmail.com>
2025-02-08 15:51:13 +0100

    Learning UI Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule,RouterModule,LoginComponent],
+  imports: [FormsModule,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
